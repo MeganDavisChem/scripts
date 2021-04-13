8911abace8dd56781e246cb19d5429a2fce1d47f
Megan Christina Davis <r2610@maple.cm.cluster>
2021-04-13 16:29:42 -0500

    t+eom stuff

--- t_eom_aee.sh
+++ t_eom_aee.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-alias summarize="/ddn/home1/r2518/Programs/summarize/summarize"
 echo "Make sure you run it in the freqs/geo/pts dir!"
-cmin=$(cd pts; new.cccr | tail -1 | awk '{print $NF}')
+cmin=$(cd pts; t_eom.cccr | tail -1 | awk '{print $NF}')
 anmin=$(cd freqs; grep -a "WHERE" anpass1.out | awk '{print $NF}')
 zpt=$(cd freqs; summarize spectro2.out | head -1 | awk '{print $2}' | cut -d= -f2) 
 echo "CcCR min: $cmin"

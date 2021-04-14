f37027454e2acb00c19a0c0cb0e67709e9fec7c6
Megan Davis <mdavis@master.cl.olemiss.edu>
2021-04-14 10:44:00 -0500

    added cc3 stuff

---
+++ cc3_aee_gs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Make sure you run it in the freqs/geo/pts dir!"
+echo "This version isn't agnostic to directory names!"
+#cmin=$(cd pts; new.cccr | tail -1 | awk '{print $NF}')
+search="CC3 total energy"
+cmin=$(cd pts/tz/inp; grep "$search" *.com.dat | awk '{print $NF}' | sort | tail -1);
+anmin=$(cd freqs; grep -a "WHERE" anpass1.out | awk '{print $NF}')
+zpt=$(cd freqs; summarize spectro2.out | head -1 | awk '{print $2}' | cut -d= -f2) 
+echo "CC3 min: $cmin"
+echo "Anpass min: $anmin"
+echo "ZPT: $zpt"
+
+echo "CccCR+Anpass+ZPT energy in cm-1"
+echo "($cmin+$anmin)*219474.63+$zpt" | bc | xargs echo

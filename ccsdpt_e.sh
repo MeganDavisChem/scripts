24b53648f4180b5f8d7cfa0f4b253fe55d59bc5a
Megan Davis <mdavis@master.cl.olemiss.edu>
2021-11-22 13:02:07 -0600

    reorganized everything

--- ccsdpt_e.sh
+++ ccsdpt_e.sh
@@ -6,4 +6,4 @@ search="CCSD(T) t"
 min=$(grep "$search" *.com.dat | awk '{print $NF}' | sort | tail -1);
 
 #Get relative energy
-grep "$search" *.com.dat | awk -v min=$min '{printf("%14.12f\n",$NF-min)}'
+grep "$search" *.com.dat | awk -v min=$min '{printf("%15.12f\n",$NF-min)}'
--- e_ccsd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Get minimum energy
-#smart way!
-min=$(grep "\!" *.out | grep "CCSD" | awk '{print $NF}' | sort | tail -1)
-
-grep "\!" *.out | grep "CCSD" | awk -v v=$min '{printf("%20.12f\n", $NF-v)}' | awk '{print $NF}'
-

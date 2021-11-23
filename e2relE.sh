03e72a8d689cad1a9c0b82756249def61932047c
Megan Christina Davis <r2610@maple.cm.cluster>
2021-11-23 10:29:55 -0600

    smarter e2rele

--- e2relE.sh
+++ e2relE.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-min=$(awk '{print $NF}' energy.dat | sort | tail -1)
-awk -v min=$min '{printf("%15.12f\n", $NF-min)}' energy.dat > relE.dat
+min=$(awk '{print $NF}' $1 | sort | tail -1)
+awk -v min=$min '{printf("%15.12f\n", $NF-min)}' $1 > rel"$1"
